eb85d2dbb5cda0077a4fe20202992c2f2c7a2061
cereceres <jesus.edelcereceres@gmail.com>
2017-11-30 20:58:28 -0600

    new version is published

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-'use strict'
-/**
- *  The module to generate the unique Id using the current date
- *  public API
- */
-let lengthVersion_1 = 30
-module.exports = function (length) {
-    // check if the lenght is given has to be a number
-    if (length && (typeof length !== 'number')) return
-    // set the lenght for ID
-    lengthVersion_1 = +(length) || lengthVersion_1
-    // get the current date
-    let now = Date.now()*Date.now()
-    // the defalt radix to use
-    let radix = 30
-    // id init
-    let id = now.toString(radix)
-    // define the recurrent function 
-    let getId = function () {
-        // radix is updated
-        radix = radix - Math.floor(Math.random() * 5)
-        // check the length
-        if (id.length < lengthVersion_1) {
-            // update id
-            id = id + now.toString(radix)
-            // call the recurrent function
-            return getId()
+
+let lengthVersion = 30;
+const mult = process.memoryUsage().heapTotal % 1000;
+module.exports = function(length) {
+    if (length && typeof length !== 'number') return;
+    lengthVersion = Number(length) || lengthVersion;
+    const now = Date.now() * process.memoryUsage().heapUsed * mult;
+    const radix = 36;
+    let id = now.toString(radix);
+    const getId = function() {
+        if (id.length < lengthVersion) {
+            id = id + now.toString(radix);
+            return getId();
         }
-        // return the id 
-        return id.slice(0,lengthVersion_1)
-    }
-    return getId()
-}
+        return id.slice(0, lengthVersion);
+    };
+    return getId();
+};
--- test/test.js
+++ test/test.js
@@ -1,17 +1,27 @@
-'use strict'
-let node_uid = require("../index")
-let assert = require("assert")
-describe('the test to uid', function () {
-    it('should get the correct length', function () {
-        assert.equal(node_uid().length, 30)
-    })
-    
-    it('should get the correct length when the length is passed', function () {
-        let length = 20 + Math.floor(Math.random()*10)
-        assert.equal(node_uid(length).length, length)
-    })
-    
-    it('should return a undefined if a number is not passed', function () {
-        assert(!node_uid('undefined'))
-    })
-})
+'use strict';
+const node_uid = require('../index');
+const assert = require('assert');
+describe('the test to uid', () => {
+    it('should get the correct length', () => {
+        assert.equal(node_uid().length, 30);
+    });
+
+    it('should get the correct length when the length is passed', () => {
+        const length = 20 + Math.floor(Math.random() * 10);
+        assert.equal(node_uid(length).length, length);
+    });
+
+    it('should return a undefined if a number is not passed', () => {
+        assert(!node_uid('undefined'));
+    });
+
+    it('should be unique', () => {
+        const set = new Set();
+        const length = 10;
+        for (let i = 0; i < 1000000000000; i++) {
+            const id = node_uid(length);
+            assert(!set.has(id));
+            set.add(id);
+        }
+    });
+});
